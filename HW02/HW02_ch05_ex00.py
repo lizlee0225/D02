84d66f213359488fdf9d0a8953fa81f710a4c83e
Hyemin Lee <Hyemin Lee>
2016-08-03 15:43:58 -0700

    completed

--- HW02/HW02_ch05_ex00.py
+++ HW02/HW02_ch05_ex00.py
@@ -6,9 +6,10 @@
 
 ################################################################################
 # Write your functions below:
-# Body
-
-
+def do_n(f, n):
+	if (n>0):
+		f()
+		do_n(f,n-1)
 
 
 
@@ -25,7 +26,7 @@ def main():
     When complete have one function call in this function:
     do_n(print_hello, 10)
     """
-    print_hello() # replace this with do_n(print_hello, 10)
+    do_n(print_hello, 10) # replace this with do_n(print_hello, 10)
 
 
 
